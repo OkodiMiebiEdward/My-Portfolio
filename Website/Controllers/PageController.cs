7948650ce9dd1f7cff09578eccd3131dd40c26be
miebiokodiedward@gmail.com <User@DESKTOP-1EK6ASL>
2023-09-25 05:55:46 -0700

    Added controller named /Page/ to the Controllers Folder

---
+++ Website/Controllers/PageController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Website.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Skill()
+        {
+            return View();
+        }
+
+        public IActionResult Contact() 
+        { 
+            return View();
+        }
+    }
+}
