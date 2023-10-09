0fc071dc4435cd54ae90db3c53e7ccf3c2b31709
Miebi <User@DESKTOP-1EK6ASL>
2023-10-09 15:31:14 -0700

    Added new Views to the Portfolio Navigation

---
+++ Website/Controllers/PortfolioController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Website.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult MVC()
+        {
+            return View();
+        }
+
+        public IActionResult Razor()
+        {
+           return View();
+        }
+
+        public IActionResult Blazor() 
+        { 
+            return View();
+        }
+
+        public IActionResult WebApi()
+        {
+            return View();
+        }
+
+    }
+}
